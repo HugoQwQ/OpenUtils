c9931e7554ccf995ddb700542d9b4ac89a35108a
polaris <dev@afterlike.org>
2025-12-10 21:59:14 -0500

    feat: add bed wars hud module

---
+++ src/main/java/org/afterlike/openutils/module/impl/minigames/BedWarsHudModule.java
@@ -0,0 +1,173 @@
+package org.afterlike.openutils.module.impl.minigames;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.afterlike.openutils.event.api.EventPhase;
+import org.afterlike.openutils.event.handler.EventHandler;
+import org.afterlike.openutils.event.impl.GameTickEvent;
+import org.afterlike.openutils.event.impl.ReceiveChatEvent;
+import org.afterlike.openutils.event.impl.RenderOverlayEvent;
+import org.afterlike.openutils.module.api.Module;
+import org.afterlike.openutils.module.api.ModuleCategory;
+import org.afterlike.openutils.module.api.hud.HudModule;
+import org.afterlike.openutils.module.api.hud.Position;
+import org.afterlike.openutils.module.api.setting.Setting;
+import org.afterlike.openutils.module.api.setting.impl.BooleanSetting;
+import org.afterlike.openutils.module.api.setting.impl.DescriptionSetting;
+import org.afterlike.openutils.util.client.ClientUtil;
+import org.afterlike.openutils.util.client.TextUtil;
+import org.afterlike.openutils.util.game.GameModeUtil;
+import org.afterlike.openutils.util.game.RenderUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class BedWarsHudModule extends Module implements HudModule {
+	private final Position position = new Position(5, 50);
+	private final DescriptionSetting disclaimer;
+	private final BooleanSetting editPosition;
+	private final BooleanSetting dropShadow;
+	private final BooleanSetting showTrap;
+	private final BooleanSetting showSharp;
+	private final BooleanSetting showProt;
+	private final List<String> trapQueue = new ArrayList<>();
+	private final List<String> protUpgrades = new ArrayList<>();
+	private final List<String> sharpUpgrades = new ArrayList<>();
+	public BedWarsHudModule() {
+		super("Bed Wars HUD", ModuleCategory.MINIGAMES);
+		disclaimer = this
+				.registerSetting(new DescriptionSetting("Hypixel language must be ENGLISH!"));
+		editPosition = this.registerSetting(new BooleanSetting("Edit position", false));
+		dropShadow = this.registerSetting(new BooleanSetting("Drop shadow", true));
+		showTrap = this.registerSetting(new BooleanSetting("Show trap", true));
+		showSharp = this.registerSetting(new BooleanSetting("Show sharpness", true));
+		showProt = this.registerSetting(new BooleanSetting("Show protection", true));
+	}
+
+	@EventHandler
+	private void onChatReceived(@NotNull final ReceiveChatEvent event) {
+		if (!ClientUtil.notNull())
+			return;
+		if (GameModeUtil.getBedWarsStatus() != 3)
+			return;
+		final String message = TextUtil.stripColorCodes(event.getMessage());
+		if (message.contains(":"))
+			return;
+		final String purchasedPattern = message
+				.substring(message.indexOf("purchased") + "purchased".length());
+		if (message.contains("purchased") && message.contains("Trap")) {
+			final String trap = purchasedPattern.replace("Trap", "").trim();
+			trapQueue.add(trap);
+		} else if (message.contains("purchased") && message.contains("Reinforced Armor")) {
+			protUpgrades.clear();
+			final String armor = purchasedPattern.replace("Reinforced Armor", "").trim();
+			protUpgrades.add(armor);
+		} else if (message.contains("purchased") && message.contains("Sharpened Swords")) {
+			sharpUpgrades.clear();
+			final String sharp = purchasedPattern.replace("Sharpened Swords", "").trim();
+			sharpUpgrades.add(sharp);
+		} else if (message.contains("Trap was set off!")) {
+			if (!trapQueue.isEmpty()) {
+				trapQueue.remove(0);
+			}
+		} else if (message.contains("Trap from the queue!")) {
+			if (!trapQueue.isEmpty()) {
+				trapQueue.remove(0);
+			}
+		}
+	}
+
+	@EventHandler
+	private void onRender(@NotNull final RenderOverlayEvent event) {
+		if (!ClientUtil.notNull())
+			return;
+		if (mc.gameSettings.showDebugInfo)
+			return;
+		if (GameModeUtil.getBedWarsStatus() != 3)
+			return;
+		int y = position.getY();
+		int delta = 0;
+		for (final String line : buildLines()) {
+			mc.fontRendererObj.drawString(line, position.getX(), y,
+					RenderUtil.getChromaColor(2L, delta), useHudDropShadow());
+			y += mc.fontRendererObj.FONT_HEIGHT + 2;
+			delta -= 90;
+		}
+	}
+
+	@EventHandler
+	private void onTick(final @NotNull GameTickEvent event) {
+		if (event.getPhase() != EventPhase.POST)
+			return;
+		if (GameModeUtil.getBedWarsStatus() != 3) {
+			resetTracking();
+		}
+	}
+
+	private @NotNull List<@NotNull String> buildLines() {
+		final List<String> lines = new ArrayList<>();
+		if (showTrap.getValue()) {
+			if (trapQueue.isEmpty()) {
+				lines.add("§rTrap: §cNone");
+			} else {
+				lines.add("§rTrap: §a" + trapQueue.get(0));
+			}
+		}
+		if (showSharp.getValue()) {
+			if (sharpUpgrades.isEmpty()) {
+				lines.add("§rSharp: §cNone");
+			} else {
+				lines.add("§rSharp: §aI");
+			}
+		}
+		if (showProt.getValue()) {
+			if (protUpgrades.isEmpty()) {
+				lines.add("§rProt: §cNone");
+			} else {
+				lines.add("§rProt: §a" + protUpgrades.get(0));
+			}
+		}
+		return lines;
+	}
+
+	private void resetTracking() {
+		trapQueue.clear();
+		protUpgrades.clear();
+		sharpUpgrades.clear();
+	}
+
+	@Override
+	protected void onEnable() {
+		resetTracking();
+	}
+
+	@Override
+	protected void onDisable() {
+		resetTracking();
+	}
+
+	@Override
+	public void onSettingChanged(@Nullable final Setting<?> setting) {
+		handleHudSettingChanged(setting);
+		super.onSettingChanged(setting);
+	}
+
+	@Override
+	public @NotNull Position getHudPosition() {
+		return position;
+	}
+
+	@Override
+	public @NotNull BooleanSetting getHudEditSetting() {
+		return editPosition;
+	}
+
+	@Override
+	public @NotNull String getHudPlaceholderText() {
+		return "Trap:-Sharp:-Prot:";
+	}
+
+	@Override
+	public boolean useHudDropShadow() {
+		return dropShadow.getValue();
+	}
+}
